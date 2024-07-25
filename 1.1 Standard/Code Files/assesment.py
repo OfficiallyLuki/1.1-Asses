a82e1f37d22b552a4f51f27d6920055350517b5f
Luki <79182198+OfficiallyLuki@users.noreply.github.com>
2024-07-26 10:30:39 +1200

    version 2 - start/needs to be tested

--- 1.1 Standard/Code Files/assesment.py
+++ 1.1 Standard/Code Files/assesment.py
@@ -2,10 +2,21 @@ from easygui import *
 
 msgbox("Welcome to this Quiz, this is designed for people inbetween the ages of 8 - 13, if you are 14 or over you should try out the Cybersmart Youth Quiz!", title = "Welcome New User!")
 
-name = enterbox("What is your name?", "Welcome to the NZ TRIVIA Game")
-age = integerbox("How old are you?", "Welcome to the NZ TRIVIA Game")
+name = enterbox("What is your name?", "Welcome to the Cybersmart Start Quiz")
+age = integerbox("How old are you?", "Welcome to the Cybersmart Start Quiz")
+total = 0
 
 while age < 8 or age > 13:
-    age = integerbox("Please enter an age from 8 to 13", "Welcome to the NZ TRIVIA Game")
+    age = integerbox("Please enter an age from 8 to 13", "Welcome to the Cybersmart Start Quiz")
     
-msgbox("Hi "  + name + "!" + "\n\nQuiz intro/rules go here", "Welcome to the NZ TRIVIA Game", "Start")
+msgbox("Hi "  + name + "!" + "\n\n The rules are simple. You will be given 3 questions to see if your Cyber Smart, If you get a question right the first try. you will get 1 point. If not, you will get two more tries. If you still get it wrong, you will be given the correct answer.", "Welcome to the Cybersmart Start Quiz", "Start now!")
+
+a = buttonbox(str(name) + " , you want to join an online gaming site. which of the following information is okay to post online? \n\n A. A nickname\n B. Your name\n C. Your email address", choices='a' 'b' 'c')
+if a == "a":
+        total = (total+1)
+        msgbox("you got your question right the first time")
+while a != "a":
+        msgbox("you got your question wrong!")
+        a = buttonbox(str(name) + " , you want to join an online gaming site. which of the following information is okay to post online? \n\n A. A nickname\n B. Your name\n C. Your email address", choices='a' 'b' 'c')
+        if a == "a":
+            total = (total)
