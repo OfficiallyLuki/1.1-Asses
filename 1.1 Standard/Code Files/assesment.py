fcf05603fca3bf05ba40a78d64bd470bc3a2cfc7
Luki <79182198+OfficiallyLuki@users.noreply.github.com>
2024-07-30 09:54:31 +1200

    finished comments

--- 1.1 Standard/Code Files/assesment.py
+++ 1.1 Standard/Code Files/assesment.py
@@ -12,37 +12,47 @@ total = 0 # Sets total to 0, resets when everyone ones the program
 
 while age < 8 or age > 13:
     age = integerbox("Please enter an age from 8 to 13", "Welcome to the Cybersmart Start Quiz")
+
+# Welcomes the user to the quiz, gives them a breif ruleset and explains what the quiz provides
     
 msgbox("Hi "  + name + "!" + "\n\n The rules are simple. You will be given 3 questions to see if your Cyber Smart, If you get a question right the first try. you will get 1 point. If not, you will get two more tries. If you still get it wrong, you will be given the correct answer.", "Welcome to the Cybersmart Start Quiz", "Start now!")
 
-a = buttonbox(str(name) + " , you want to join an online gaming site. Which of the following information is okay to post online? \n\n A. A nickname\n B. Your name\n C. Your email address", choices='a' 'b' 'c')
-if a == "a":
-        total = (total+1)
-        msgbox(str(name) + " , you got your question right the first time")
-while a != "a":
-        msgbox(str(name) + " , you got your question wrong!")
-        a = buttonbox(str(name) + " , you want to join an online gaming site. which of the following information is okay to post online? \n\n A. A nickname\n B. Your name\n C. Your email address", choices='a' 'b' 'c')
-        if a == "a":
+# First Question to the quiz
+
+a = buttonbox(str(name) + " , you want to join an online gaming site. Which of the following information is okay to post online? \n\n A. A nickname\n B. Your name\n C. Your email address", choices='a' 'b' 'c') # Provides the user with the question and three choices
+if a == "a": # If the user gets the answer on the first try they following will run;
+        total = (total+1) # Gives user a point for getting the question right on the first time
+        msgbox(str(name) + " , you got your question right the first time") # Provides the user with the information that they got the question right on the first time
+while a != "a": # Repeats the question until they get it wrong
+        msgbox(str(name) + " , you got your question wrong!") # If the user gets it wrong they'll be presented with this
+        a = buttonbox(str(name) + " , you want to join an online gaming site. which of the following information is okay to post online? \n\n A. A nickname\n B. Your name\n C. Your email address", choices='a' 'b' 'c') # Repeated Question
+        if a == "a": # Sets the total to neutral/adds no points since they got the question right but not on the first time
             total = (total)
 
-c = buttonbox(str(name) + " , Someone in your class has posted their first video on YouTube and has asked you to comment on it. You don't think the video is good because the editing is very choppy. What could you comment? \n\n A. Your video is rubbish!\n B. Man, this is awful! Stick to playing sport or something.\n C. Congrats on your first video! Let me know if you'd like any help editing for your next video.", choices='a' 'b' 'c')
-if c == "c":
-        total = (total+1)
-        msgbox(str(name) + " , you got your question right the first time")
-while c != "c":
-        msgbox(str(name) + ", you got your question wrong!")
-        c = buttonbox(str(name) + " , Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do? \n\n A. Delete the message and try forget about it\n B. Keep the text and show an adult you trust\n C. Text the person back saying something mean to them", choices='a' 'b' 'c')
+# Second Question of the quiz
+
+c = buttonbox(str(name) + " , Someone in your class has posted their first video on YouTube and has asked you to comment on it. You don't think the video is good because the editing is very choppy. What could you comment? \n\n A. Your video is rubbish!\n B. Man, this is awful! Stick to playing sport or something.\n C. Congrats on your first video! Let me know if you'd like any help editing for your next video.", choices='a' 'b' 'c') # Provides the user with the question and three choices
+if c == "c": # If the user gets the answer on the first try they following will run;
+        total = (total+1) # Gives user a point for getting the question right on the first time
+        msgbox(str(name) + " , you got your question right the first time") # Provides the user with the information that they got the question right on the first time
+while c != "c":  # Repeats the question until they get it wrong
+        msgbox(str(name) + ", you got your question wrong!")  # If the user gets it wrong they'll be presented with this
+        c = buttonbox(str(name) + " , Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do? \n\n A. Delete the message and try forget about it\n B. Keep the text and show an adult you trust\n C. Text the person back saying something mean to them", choices='a' 'b' 'c') # Repeated Question
         if c == "c":
-            total = (total)
+            total = (total) # Sets the total to neutral/adds no points since they got the question right but not on the first time
 
-b = buttonbox(str(name) + " , Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do? \n\n A. Delete the message and try forget about it\n B. Keep the text and show an adult you trust\n C. Text the person back saying something mean to them", choices='a' 'b' 'c')
-if b == "b":
-        total = (total+1)
-        msgbox(str(name) + " , you got your question right the first time")
-while b != "b":
-        msgbox(str(name) + " , you got your question wrong!")
-        b = buttonbox(str(name) + " , Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do? \n\n A. Delete the message and try forget about it\n B. Keep the text and show an adult you trust\n C. Text the person back saying something mean to them", choices='a' 'b' 'c')
+# Third Question of the quiz
+
+b = buttonbox(str(name) + " , Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do? \n\n A. Delete the message and try forget about it\n B. Keep the text and show an adult you trust\n C. Text the person back saying something mean to them", choices='a' 'b' 'c') # Provides the user with the question and three choices
+if b == "b": # If the user gets the answer on the first try they following will run;
+        total = (total+1)  # Gives user a point for getting the question right on the first time
+        msgbox(str(name) + " , you got your question right the first time") # Provides the user with the information that they got the question right on the first time
+while b != "b": # Repeats the question until they get it wrong
+        msgbox(str(name) + " , you got your question wrong!")   # If the user gets it wrong they'll be presented with this
+        b = buttonbox(str(name) + " , Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do? \n\n A. Delete the message and try forget about it\n B. Keep the text and show an adult you trust\n C. Text the person back saying something mean to them", choices='a' 'b' 'c') # Repeated Question
         if b == "b":
-            total = (total)
+            total = (total) # Sets the total to neutral/adds no points since they got the question right but not on the first time
+
+# Ends the quiz with the following msgbox, also presents the user with the total that they got
 
 msgbox(str(name) + " well done! You've finished the quiz, you got " + str(total) + "/3 correct! If you want to you can retry this quiz, or move onto the next quiz for 14+ Cybersmart Youth Quiz, you can!.")
